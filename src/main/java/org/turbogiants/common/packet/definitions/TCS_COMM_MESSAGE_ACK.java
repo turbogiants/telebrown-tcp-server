888911c16a2c5520e283473f23b75b1cea527cf3
Raitou <raitoukun@hotmail.com>
2021-10-20 14:04:42 +0800

    Use Server Time for Messages instead of basing it from the client

--- src/main/java/org/turbogiants/common/packet/definitions/TCS_COMM_MESSAGE_ACK.java
+++ src/main/java/org/turbogiants/common/packet/definitions/TCS_COMM_MESSAGE_ACK.java
@@ -1,11 +1,12 @@
 package org.turbogiants.common.packet.definitions;
 
+import org.turbogiants.common.MessageInfo;
 import org.turbogiants.common.packet.InPacket;
 import org.turbogiants.common.packet.OutPacket;
 import org.turbogiants.common.packet.PacketEnum;
 
 
-public class TCS_COMM_MESSAGE_ACK {
+public class TCS_COMM_MESSAGE_ACK extends TCS_COMM_MESSAGE_NOT{
 
     public enum Status{
         MESSAGE_SENT_FAILED(-1),
@@ -42,14 +43,26 @@ public class TCS_COMM_MESSAGE_ACK {
         return iOK;
     }
 
+    @Override
     public OutPacket serialize(PacketEnum header){
         OutPacket outPacket = new OutPacket(header);
         outPacket.encodeInt(iOK);
+        outPacket.encodeInt(messageInfo.getDestID());
+        outPacket.encodeInt(messageInfo.getOwnerID());
+        outPacket.encodeLong(messageInfo.getUnixTime());
+        outPacket.encodeString(messageInfo.getMessage());
         return outPacket;
     }
 
+    @Override
     public void deserialize(InPacket inPacket){
         iOK = inPacket.decodeInt();
+        MessageInfo messageInfo = new MessageInfo();
+        messageInfo.setDestID(inPacket.decodeInt());
+        messageInfo.setOwnerID(inPacket.decodeInt());
+        messageInfo.setUnixTime(inPacket.decodeLong());
+        messageInfo.setMessage(inPacket.decodeString());
+        this.messageInfo = messageInfo;
     }
 
 }
